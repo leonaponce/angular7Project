f97cfba032f370991fb589a6253bc9b9a78ca305
Leona Ponce <42949063+leonaponce@users.noreply.github.com>
2018-12-21 23:53:46 -0500

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-server></app-server><app-server></app-server>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
