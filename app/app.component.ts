fd939ecc5be7e4b72f34f9cb126cec6f18b29b06
Leona Ponce <42949063+leonaponce@users.noreply.github.com>
2018-12-29 12:41:29 -0500

    Add files via upload

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'aIMLResourcesApp';
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent } from './header/header.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ResourcesListComponent } from './resources/resources-list/resources-list.component';
+import { ResourcesDetailsComponent } from './resources/resources-details/resources-details.component';
+import { ResourcesItemComponent } from './resources/resources-list/resources-item/resources-item.component';
+import { ResourceRoadmapComponent } from './resource-roadmap/resource-roadmap.component';
+import { ResourceEditComponent } from './resource-roadmap/resource-edit/resource-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    HeaderComponent,
+    ResourcesComponent,
+    ResourcesListComponent,
+    ResourcesDetailsComponent,
+    ResourcesItemComponent,
+    ResourceRoadmapComponent,
+    ResourceEditComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
